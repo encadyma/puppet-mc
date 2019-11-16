21a95c943d0085216f9b850b1047f4ffdefc421b
Kevin Mo <encadyma@gmail.com>
2019-11-16 08:40:07 +0000

    Config so far: backups need fixing

---
+++ backup.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+cp -r /home/minecraft/world "/home/minecraft/backups/world-$(date -Is)"
--- minecraft.pp
+++ minecraft.pp
@@ -26,3 +26,46 @@ file { '/home/minecraft/server.jar':
 	source => 'https://launcher.mojang.com/v1/objects/3dc3d84a581f14691199cf6831b71ed1296a9fdf/server.jar',
 	owner => 'minecraft',
 }
+
+$memory_available = $facts['memory']['system']['available_bytes'] / 2
+
+file { '/etc/systemd/system/minecraft.service':
+	content => template('/home/kmo/minecraft/minecraft.service.erb'),
+	owner => 'minecraft',
+}
+
+service { 'minecraft':
+	name => 'minecraft',
+	ensure => running,
+}
+
+file { '/home/minecraft/backups':
+	ensure => 'directory',
+	owner => 'minecraft',
+}
+
+file { '/home/minecraft/backup.sh':
+	source => '/home/kmo/minecraft/backup.sh',
+	owner => 'minecraft',
+	mode => '600',
+}
+
+cron { 'mcbackup':
+	command => 'source /home/minecraft/backup.sh',
+	user => 'minecraft',
+	minute => '*',
+}
+
+exec { 'swapstuff':
+	command => 'dd if=/dev/zero of=/swapfile count=4G',
+	creates => '/swapfile',
+	user => 'root',
+	path => ["/bin", "/usr/bin", "/sbin", "/usr/sbin"]
+}
+
+exec { 'swapwhoop':
+	command => 'mkswap /swapfile && swapon /swapfile',
+	unless => 'swapon -s | grep swapfile',
+	user => 'root',
+	path => ["/bin", "/usr/bin", "/sbin", "/usr/sbin"]
+}
